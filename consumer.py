b4b4152f0fd2eb68255ccdc4cec5c687ea585665
jeongmin <silverstar456@yonsei.ac.kr>
2022-03-03 16:11:10 +0000

    컨슈머에서 이미지 추론까지 성공

--- consumer.py
+++ consumer.py
@@ -1,19 +1,21 @@
 from kafka import KafkaConsumer
 import cv2
 import numpy as np
-
+import io
+from PIL import Image
+import base64
+from utils import predict
+import onnxruntime
 
 bootstrap_servers = ["34.125.104.58:9091", "34.125.104.58:9092", "34.125.104.58:9093"]
 consumer = KafkaConsumer("VideoStream", bootstrap_servers=bootstrap_servers)
 
-for message in consumer:
-    # print(message.value)
-    # print(dir(message))
-    # message.decode("utf-8")
-    nparr = np.frombuffer(message.value, dtype=np.uint8)
+for msg_idx, message in enumerate(consumer):
+    np_arr = np.frombuffer(message.value, dtype=np.uint8)
+    cv2_img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
 
-    # decode image
-    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-    img = cv2.resize(img, (224, 224))
+    results = predict(cv2_img)
+    print(results)
 
-    print(img.shape)
+    if msg_idx == 1:
+        break
