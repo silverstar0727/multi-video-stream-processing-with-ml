a206acdbc43d615790feb5bdc89ad080a9e73a7e
jeongmin <silverstar456@yonsei.ac.kr>
2022-03-03 16:29:45 +0000

    rename producer.py to producer_file.py

---
+++ producer_cam.py
@@ -0,0 +1,28 @@
+
+producer = KafkaProducer(bootstrap_servers='localhost:9092')
+topic = 'my-topic'
+
+
+def emit_video(path_to_video):
+    print('start')
+
+    video = cv2.VideoCapture(path_to_video)
+
+    while video.isOpened():
+        success, frame = video.read()
+        if not success:
+            break
+
+        # png might be too large to emit
+        data = cv2.imencode('.jpeg', frame)[1].tobytes()
+
+        future = producer.send(topic, data)
+        try:
+            future.get(timeout=10)
+        except KafkaError as e:
+            print(e)
+            break
+
+        print('.', end='', flush=True)
+
+emit_video(0)
