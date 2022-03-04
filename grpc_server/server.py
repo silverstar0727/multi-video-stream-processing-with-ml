0bd915e218643b5d38c62979f4a82b94f7c56905
jeongmin <silverstar456@yonsei.ac.kr>
2022-03-04 14:24:44 +0000

    용량 제한 해제하기

--- grpc_server/server.py
+++ grpc_server/server.py
@@ -14,14 +14,24 @@ class Greeter(yolox_pb2_grpc.YoloxServicer):
 
     def Inference(self, request, context):
         results = predict(request.b64image)
+        print("success")
         # encoded_results = base64.b64encode(results)
 
         # return yolox_pb2.Prediction(bbox_arr=results) # 결과 반환
 
 def serve():
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    options = [
+        # ('grpc.max_send_message_length', 1024 * 1024 * 1024), 
+        ('grpc.max_receive_message_length', 1024 * 1024 * 1024 )
+    ]
+    server = grpc.server(
+        futures.ThreadPoolExecutor(max_workers=10),
+        options=options
+    )
+
     yolox_pb2_grpc.add_YoloxServicer_to_server(Greeter(), server)
     server.add_insecure_port('[::]:50051')
+    
     server.start()
     server.wait_for_termination()
 
