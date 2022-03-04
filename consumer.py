553a58ab352cc454a2cc59478fe7fff95a7dfa9c
jeongmin <silverstar456@yonsei.ac.kr>
2022-03-04 05:25:33 +0000

    inference 타입으로 분리

--- consumer.py
+++ consumer.py
@@ -8,11 +8,17 @@ from utils import predict
 bootstrap_servers = ["34.125.104.58:9091", "34.125.104.58:9092", "34.125.104.58:9093"]
 consumer = KafkaConsumer("VideoStream", bootstrap_servers=bootstrap_servers)
 
-for msg_idx, message in enumerate(consumer):
-    np_arr = np.frombuffer(message.value, dtype=np.uint8)
-    cv2_img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+inference_type = "local"
 
-    results = predict(cv2_img)
+if inference_type == "local":
+    for msg_idx, message in enumerate(consumer):
+        np_arr = np.frombuffer(message.value, dtype=np.uint8)
+        cv2_img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
 
-    if msg_idx == 1:
-        break
+        results = predict(cv2_img)
+
+if inference_type == "grpc":
+    pass
+
+if inference_type == "rest":
+    pass
