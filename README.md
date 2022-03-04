e1fb155a730bfbd85fb33561034cad2da45a084b
jeongmin <silverstar456@yonsei.ac.kr>
2022-03-04 12:57:29 +0000

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+### kafka를 이용한 video inference
+
+~~~sh
+docker-compose up
+
+pip install -r requirements.txt
+
+python producer_file.py
+python consumer.py
+~~~
--- consumer.py
+++ consumer.py
@@ -1,13 +1,19 @@
-from kafka import KafkaConsumer
+from kafka import KafkaConsumer, KafkaProducer
 
 import cv2
 import numpy as np
-import base64
+import json
 
-from utils import predict, grpc_call
+from utils import predict
 
 bootstrap_servers = ["34.125.104.58:9091", "34.125.104.58:9092", "34.125.104.58:9093"]
 consumer = KafkaConsumer("VideoStream", bootstrap_servers=bootstrap_servers)
+producer = KafkaProducer(bootstrap_servers=bootstrap_servers)
 
 for msg_idx, message in enumerate(consumer):
     results = predict(message.value)
+    encoded_results = json.dumps(results).encode('utf-8')
+
+    producer.send("ResultStream", encoded_results)
+
+producer.flush()
