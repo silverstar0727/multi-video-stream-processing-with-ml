104344157e5f80deb4259d7bb691b48b1c2fe4bd
jeongmin <silverstar456@yonsei.ac.kr>
2022-03-04 12:05:28 +0000

    케이스에 맞게 설정

--- consumer.py
+++ consumer.py
@@ -3,22 +3,20 @@ from kafka import KafkaConsumer
 import cv2
 import numpy as np
 
-from utils import predict
+from utils import predict, grpc_call
 
 bootstrap_servers = ["34.125.104.58:9091", "34.125.104.58:9092", "34.125.104.58:9093"]
 consumer = KafkaConsumer("VideoStream", bootstrap_servers=bootstrap_servers)
 
-inference_type = "local"
+inference_type = "grpc"
 
 if inference_type == "local":
     for msg_idx, message in enumerate(consumer):
-        np_arr = np.frombuffer(message.value, dtype=np.uint8)
-        cv2_img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
-
-        results = predict(cv2_img)
+        results = predict(message.value)
 
 if inference_type == "grpc":
-    pass
+    for msg_idx, message in enumerate(consumer):
+        results = grpc_call(message.value)
 
 if inference_type == "rest":
     pass
