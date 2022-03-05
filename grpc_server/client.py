da2d0569aa7979623da59c915ae7a07ce0b97b17
jeongmin <silverstar456@yonsei.ac.kr>
2022-03-05 07:02:16 +0000

    url 하드코딩 수정

--- grpc_server/client.py
+++ grpc_server/client.py
@@ -4,12 +4,12 @@ import yolox_pb2, yolox_pb2_grpc
 import cv2
 import base64
 
-def grpc_call(img_arr):
+def grpc_call(img_arr, grpc_server_url="localhost:50051"):
     options = [
         ('grpc.max_send_message_length', 1024 * 1024 * 1024), 
         # ('grpc.max_receive_message_length', 1024 * 1024 * 1024 )
     ]
-    channel = grpc.insecure_channel("localhost:50051", options)
+    channel = grpc.insecure_channel(grpc_server_url, options)
     stub = yolox_pb2_grpc.YoloxStub(channel)
 
     data = base64.b64encode(img_arr)
